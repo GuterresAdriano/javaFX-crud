28ee2ad54b33cbaf57a7b99b2a4b47581dbba5a0
Adriano <guterres.adriano@gmail.com>
2020-03-05 17:42:45 -0300

    Instanciação da camada DAO

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,38 +1,46 @@
 package model.entities;
 
-public class Department {
-	
-	private int id;
+import java.io.Serializable;
+
+public class Department implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
 	private String name;
 	
-	public Department(int id, String name) {
+	public Department() {
+	}
+
+	public Department(Integer id, String name) {
 		this.id = id;
 		this.name = name;
 	}
-	public int getId() {
+
+	public Integer getId() {
 		return id;
 	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	@Override
-	public String toString() {
-		return "Department [id=" + id + ", name=" + name + "]";
-	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -42,16 +50,16 @@ public class Department {
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
+		if (id == null) {
+			if (other.id != null)
 				return false;
-		} else if (!name.equals(other.name))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
-	
-	
 
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + "]";
+	}
 }
