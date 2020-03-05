0b30511c90640bb3cec93bc61e284ecd243f5f0f
Adriano <guterres.adriano@gmail.com>
2020-03-05 18:12:29 -0300

    Implementação da janela de cadastro de departamento

--- src/controller/DepartmentListController.java
+++ src/controller/DepartmentListController.java
@@ -1,18 +1,30 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Tools;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -34,8 +46,9 @@ public class DepartmentListController implements Initializable {
 	
 	
 	@FXML
-	public void onNewButtonAction() {
-		System.out.println("onNewButtonAction");
+	public void onNewButtonAction(ActionEvent event) {
+		Stage parentStage = Tools.currentStage(event);
+		createDialogForm(parentStage, "DepartmentForm");
 	}
 
 	@Override
@@ -58,11 +71,29 @@ public class DepartmentListController implements Initializable {
 	public void updateTableView() {
 		if(service== null) {
 			throw new IllegalStateException("Service is null");
-		}
-		
+		}		
 		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		departmentTableView.setItems(obsList);
+	}
+	
+	private void createDialogForm(Stage parentStage, String absoluteName) {
+		try {
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/"+absoluteName+ ".fxml"));			
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);	
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();			
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}	
 	}
 
 }
